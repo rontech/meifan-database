e80bc1ffd56a9264f17b0654f613e8c7d1f9c2c4
jie-zhang <jie-zhang@rontech.co.jp>
2015-04-29 17:50:04 +0800

    Modified the database restore script for backuping before restoring.

--- conf/mongorestore.sh
+++ conf/mongorestore.sh
@@ -12,6 +12,14 @@ read answer
 
 if [ $answer = "y" -o $answer = "Y" ]
 then
+  # backup
+  mkdir -p dbackup
+  dt=`date +"%Y%m%d%H%M%S"`
+  mongodump -hlocalhost -o ./dbackup/$dt
+  gzip -cr ./dbackup/$dt > ./dbackup/$dt.gz
+  rm -rvf ./dbackup/$dt
+
+  # restore
   mongorestore -hlocalhost ./sampledb --drop
   echo "OK."
 else
